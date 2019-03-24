77e2d2424dc6f61b1c6ee8c41e16eb554a571c63
deepakskosanam <deepakskosanam@users.noreply.github.com>
2019-03-24 15:16:38 -0400

    Starting Tutorials

--- tutorials/test.py
+++ tutorials/test.py
@@ -1,2 +1,5 @@
 #TEST
 #Test Number 2
+the_world_is_flat = True
+if the_world_is_flat:
+    print("Be careful not to fall off!")
---
+++ tutorials/tutorial_3.py
@@ -0,0 +1,13 @@
+# TUTORIALS
+
+# this is the first comment
+spam = 1    # and this is the second comment
+            # ... and now a third!
+text = "# This is not a comment because it's inside quotes."
+
+
+# Numbers
+2 + 2
+50 - 5*6
+(50- 5*6) / 4
+8 / 5
